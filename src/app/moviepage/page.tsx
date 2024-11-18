61332bff0c4af4f3d2a5eac3ba4d972ba9bb2d4d
yuunhae <susu492@naver.com>
2024-11-19 08:14:31 +0900

    영화 포스터 및 제목 화면 로드 완료

--- src/app/moviepage/page.tsx
+++ src/app/moviepage/page.tsx
@@ -1,16 +1,59 @@
+'use client'
+
 import StyledFont from "./components/StyledFont";
 import StyledColumn from "./components/StyledColumn";
+import StyledGrid from "./components/StyledGrid";
+import { useEffect, useState } from "react";
+import { fetchNowPlayingMovies } from "@/lib/apiClient";
+import styled from "styled-components";
 
 export default function MoviePage() {
+	const [movies, setMovies] = useState<any[]>([]); //영화 데이터를 저장할 상태 변수 
+	const imageBaseUrl = 'https://image.tmdb.org/t/p/w500' //이미지 URL 기본 경로
 
-	// apiClient 파일에서 정의한 API 호출 함수를 여기서 실행!
+	const MovieContainer = styled.div`
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 10px;
+		width: 100%;
+	`
+	const MovieTitle = styled.div`
+		color: #fff;
+		font-weight: bold;
+		margin-top: 10px;
+		font-size: 20px;
+	`
+
+	useEffect(() => {
+		async function loadMovies() {
+			try {
+				const moviewData = await fetchNowPlayingMovies();
+				setMovies(moviewData);
+				console.log(moviewData);
+			} catch (error) {
+				console.error(error)
+			}}
+		loadMovies();
+	}, [])
 
 	return (
 		<StyledColumn $width="100%" $alignitems="center" $justifycontent="center" $gap="1rem">
 			<StyledFont $color="white" $font='1.5rem'>
-				영화 페이지
+				상영중인 영화
 			</StyledFont>
 			{/* 여기에 /movie/now_playing API의 result를 그리드 형태로 반복출력! */}
+			<StyledGrid $columns="1fr 1fr 1fr" $gap="10px">
+				{movies.map((movie) => (
+					<MovieContainer key={movie.id}>
+						<img src={`${imageBaseUrl}/${movie.poster_path}`} width='50%'/>
+						<MovieTitle>
+							{movie.original_title}
+						</MovieTitle>
+					</MovieContainer>
+				))}
+			</StyledGrid>
 		</StyledColumn>
 	);
 }
--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,3 +1,16 @@
 export async function fetchNowPlayingMovies() {
-	// 여기에 /movie/now_playing API를 호출하는 함수를 구현!
+	const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/movie/now_playing`, {
+		method:'GET',
+		headers : {
+			'Authorization' : `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
+			'Content-Type' : 'application/json',
+		},
+	});
+
+	if(!response.ok) {
+		throw new Error('Failed to fetch movies');
+	}
+
+	const data = await response.json();
+	return data.results;
 }
